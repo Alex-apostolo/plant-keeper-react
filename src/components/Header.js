015ef5c230b796f93d51250084b0448644623379
Alex-apostolo <alex-apostolo@hotmail.com>
2021-06-17 13:27:48 +0300

    Fixed bug with navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,37 @@
 import logo from "../assets/logo.png";
-import { Navbar, Container, Nav, Image } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-        <Navbar.Brand href=" ">
-          <LinkContainer to="/">
-            <Image src={logo} width="150" alt="logo" fluid className="ml-4"/>
-          </LinkContainer>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <LinkContainer to="/listings">
-                <Nav.Link>Listings</Nav.Link>
-              </LinkContainer>
-            </Nav.Item>
+    <Navbar bg="light" expand="md">
+      <Navbar.Brand as={Link} to="/">
+        <Image className="ml-4" src={logo} width="150" alt="logo" fluid />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item> 
+            <Nav.Link as={Link} to="/listings">Listings</Nav.Link>
+          </Nav.Item>
 
-            <Nav.Item>
-              <LinkContainer to="/create-listing">
-                <Nav.Link>Create Listing</Nav.Link>
-              </LinkContainer>
-            </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/create-listing">Create Listing</Nav.Link>
+          </Nav.Item>
 
-            <Nav.Item>
-              <LinkContainer to="/login">
-                <Nav.Link>Login</Nav.Link>
-              </LinkContainer>
-            </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav.Item>
 
-            <Nav.Item>
-              <LinkContainer to="/signup">
-                <Nav.Link>Sign Up</Nav.Link>
-              </LinkContainer>
-            </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+          </Nav.Item>
 
-            <Nav.Item>
-              <LinkContainer to="/help">
-                <Nav.Link>Help</Nav.Link>
-              </LinkContainer>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/help">Help</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
