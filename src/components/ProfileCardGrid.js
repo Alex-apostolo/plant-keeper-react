af4e0df6f78cc01c18c198157314ea11663fdcde
Alex-apostolo <alex-apostolo@hotmail.com>
2021-06-19 20:58:11 +0300

    Moved fetch function to ProfileCardGrid component from Home

--- src/components/ProfileCardGrid.js
+++ src/components/ProfileCardGrid.js
@@ -1,19 +1,35 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProfileCard from "./ProfileCard";
 
-const ProfileCardGrid = (props) => {
+const ProfileCardGrid = () => {
+  const number = 48;
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=${number}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setUsers(data.results);
+      });
+  }, []);
+
+  if (loading || users.length === 0) return <div>Fetching users</div>;
+
   return (
     <Container className="p-4 mt-2" fluid>
       <Row>
-        {props.profiles.map((profile) => {
+        {users.map((user) => {
           return (
-            <Col key={profile.id} className="d-flex justify-content-center mt-3 mb-3">
+            <Col key={user.login.uuid} className="d-flex justify-content-center mt-3 mb-3">
               <ProfileCard
-                image={profile.image}
-                fullName={profile.fullName}
-                city={profile.city}
-                country={profile.country}
-                rating={profile.rating}
+                image={user.picture.large}
+                fullName={user.name.first + " " + user.name.last}
+                city={user.location.city}
+                country={user.location.countryy}
+                rating={5}
               />
             </Col>
           );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,11 @@
-import { useState, useEffect } from "react";
 import Searchbar from "../components/SearchBar";
 import ProfileCardGrid from "../components/ProfileCardGrid";
 
 const Home = () => {
-  const number = 48;
-  const [loading, setLoading] = useState(true);
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://randomuser.me/api/?results=${number}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setLoading(false);
-        setUsers(data.results);
-      });
-  }, []);
-
-  if (loading || users.length === 0) return <div>Fetching users</div>;
-
-  const profiles = users.map((user) => {
-    return {
-      id: user.login.uuid,
-      image: user.picture.large,
-      fullName: user.name.first + " " + user.name.last,
-      city: user.location.city,
-      country: user.location.country,
-      rating: 5
-    }
-  })
   return (
     <div>
       <Searchbar />
-      <ProfileCardGrid profiles={profiles} />
+      <ProfileCardGrid />
     </div>
   );
 };
